f6d15148f3214ca25480f6cd53b174322324ee7f
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2018-04-06 16:18:23 -0500

    Clean up

--- Analyzer/src/AnalyzeEventSelection.cc
+++ Analyzer/src/AnalyzeEventSelection.cc
@@ -138,15 +138,7 @@ void AnalyzeEventSelection::Loop(NTupleReader& tr, double weight, int maxevents,
         const int& ntops_2jet = tr.getVar<int>("ntops_2jet");
         const int& ntops_1jet = tr.getVar<int>("ntops_1jet");
         const std::string& runtype = tr.getVar<std::string>("runtype");
-        const std::vector<TLorentzVector>& Muons     = tr.getVec<TLorentzVector>("Muons");
-        const std::vector<TLorentzVector>& Electrons = tr.getVec<TLorentzVector>("Electrons");
         const std::vector<TLorentzVector>& Jets      = tr.getVec<TLorentzVector>("Jets");
-        const std::vector<int>&  Muons_charge        = tr.getVec<int>("Muons_charge");
-        const std::vector<int>&  Electrons_charge    = tr.getVec<int>("Electrons_charge");
-        const std::vector<bool>& Electrons_tightID   = tr.getVec<bool>("Electrons_tightID");
-        const std::vector<bool>& Electrons_passIso   = tr.getVec<bool>("Electrons_passIso");
-        const std::vector<bool>& Muons_passIso       = tr.getVec<bool>("Muons_passIso");
-        const std::vector<double>&      Jets_bDiscriminatorCSV = tr.getVec<double>("Jets_bDiscriminatorCSV");
         const std::vector<std::string>& TriggerNames           = tr.getVec<std::string>("TriggerNames");
         const std::vector<int>&         TriggerPass            = tr.getVec<int>("TriggerPass");
         const TopTaggerResults* ttr         = tr.getVar<TopTaggerResults*>("ttr");
