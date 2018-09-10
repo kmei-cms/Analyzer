54668e00b6597639b26daf6595131d453f9263fd
Kelvin Mei <kmei@princeton.edu>
2018-09-10 15:04:12 -0500

    Added new histogram making code

--- Analyzer/test/MyAnalysis.C
+++ Analyzer/test/MyAnalysis.C
@@ -11,6 +11,7 @@
 #include "Analyzer/Analyzer/include/AnalyzeStealthTopTagger.h"
 #include "Analyzer/Analyzer/include/AnalyzeBTagSF.h"
 #include "Analyzer/Analyzer/include/CalculateBTagSF.h"
+#include "Analyzer/Analyzer/include/MakeNJetDists.h"
 
 #include "SusyAnaTools/Tools/BTagCalibrationStandalone.h"
 #include "SusyAnaTools/Tools/BTagCorrector.h"
@@ -171,7 +172,7 @@ int main(int argc, char *argv[])
     int opt, option_index = 0;
     bool doBackground = false, doTopTagger = false, doEventSelection = false, 
         doEventShape = false, do0Lep = false, do1Lep = false, doStealthTT = false,
-        doBTagSF = false, calcBTagSF = false;
+        doBTagSF = false, calcBTagSF = false, makeNJetDists = false;
     bool runOnCondor = false;
     bool isSkim = false;
     std::string histFile = "", dataSets = "";
@@ -187,6 +188,7 @@ int main(int argc, char *argv[])
         {"doStealthTT",        no_argument, 0, 'x'},
         {"calcBTagSF",         no_argument, 0, 'f'},
         {"doBTagSF",           no_argument, 0, 'g'},
+        {"makeNJetDists",      no_argument, 0, 'n'},
         {"condor",             no_argument, 0, 'c'},
         {"histFile",     required_argument, 0, 'H'},
         {"dataSets",     required_argument, 0, 'D'},
@@ -195,7 +197,7 @@ int main(int argc, char *argv[])
         {"numEvts",      required_argument, 0, 'E'},
     };
 
-    while((opt = getopt_long(argc, argv, "btspzoxfgcH:D:N:M:E:", long_options, &option_index)) != -1)
+    while((opt = getopt_long(argc, argv, "btspzoxfgncH:D:N:M:E:", long_options, &option_index)) != -1)
     {
         switch(opt)
         {
@@ -208,6 +210,7 @@ int main(int argc, char *argv[])
             case 'x': doStealthTT      = true;              break;
             case 'f': calcBTagSF       = true;              break;
             case 'g': doBTagSF         = true;              break;
+            case 'n': makeNJetDists    = true;              break;
             case 'c': runOnCondor      = true;              break;
             case 'H': histFile         = optarg;            break;
             case 'D': dataSets         = optarg;            break;
@@ -239,6 +242,7 @@ int main(int argc, char *argv[])
         {doStealthTT,      run<AnalyzeStealthTopTagger>},
         {doBTagSF,         run<AnalyzeBTagSF>},
         {calcBTagSF,       run<CalculateBTagSF>},
+        {makeNJetDists,    run<MakeNJetDists>},
     }; 
     
     try
