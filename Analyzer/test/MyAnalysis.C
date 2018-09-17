825d565cb0042a4a8093bbe0fe74f1812ad0612d
Kelvin Mei <kmei@princeton.edu>
2018-09-17 13:54:47 -0500

    Changed path of histogram files to work with eos (need to fix)

--- Analyzer/test/MyAnalysis.C
+++ Analyzer/test/MyAnalysis.C
@@ -123,9 +123,10 @@ template<typename Analyze> void run(std::set<AnaSamples::FileSummary> vvf,
 
         if( runtype == "MC" ) 
         {
+            //std::string eosPath =   "root://cmseos.fnal.gov//store/user/lpcsusyhad/StealthStop/ScaleFactorHistograms/";
             BTagCorrector bTagCorrector("allInOne_BTagEff.root","", false, file.tag);
             Pileup_Sys pileup("PileupHistograms_0121_69p2mb_pm4p6.root");
-            ScaleFactors scaleFactors("allINone_leptonSF_Moriond17.root");
+            ScaleFactors scaleFactors("allInOne_leptonSF_Moriond17.root");
             tr.registerFunction( std::move(bTagCorrector) );
             tr.registerFunction( std::move(pileup) );
             tr.registerFunction( std::move(scaleFactors) );
