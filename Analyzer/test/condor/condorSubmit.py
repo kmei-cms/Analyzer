8ff19bc12c5ab0bfb699a2e13bcab51656b47b53
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2018-09-18 15:10:41 -0500

    Adding files needed now for scale factor

--- Analyzer/test/condor/condorSubmit.py
+++ Analyzer/test/condor/condorSubmit.py
@@ -52,6 +52,10 @@ filestoTransfer = [environ["CMSSW_BASE"] + "/src/%s/test/MyAnalysis" % repo,
                    environ["CMSSW_BASE"] + "/src/%s/test/sampleCollections.cfg" % repo,
                    environ["CMSSW_BASE"] + "/src/%s/test/DeepEventShape.cfg" % repo,
                    environ["CMSSW_BASE"] + "/src/%s/test/%s" % (repo,ESMVAFileName),
+                   environ["CMSSW_BASE"] + "/src/%s/test/PileupHistograms_0121_69p2mb_pm4p6.root" % repo,
+                   environ["CMSSW_BASE"] + "/src/%s/test/allInOne_BTagEff.root" % repo,
+                   environ["CMSSW_BASE"] + "/src/%s/test/allInOne_leptonSF_Moriond17.root" % repo,
+                   environ["CMSSW_BASE"] + "/src/%s/test/CSVv2_Moriond17_B_H.csv" % repo,
                    ]
 
 def makeExeAndFriendsTarball(filestoTransfer, fname):
