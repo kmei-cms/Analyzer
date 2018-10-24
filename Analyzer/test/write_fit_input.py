16d6d915e2b28a63d91978251006b8889aca8c48
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2018-10-24 12:10:22 -0500

    Cleaned up writingHistoClass now will work on DataCard class

--- Analyzer/test/write_fit_input.py
+++ Analyzer/test/write_fit_input.py
@@ -3,7 +3,17 @@ ROOT.gStyle.SetOptStat(0)
 from math import sqrt
 import random
 
-class MakeNJetPlots:
+class DataSetInfo:
+    def __init__(self, basedir, fileName, label):
+        self.basedir = basedir
+        self.fileName = fileName
+        self.label = label
+        self.file = ROOT.TFile.Open(basedir+fileName)
+
+    def __del__(self):
+        self.file.Close()
+
+class WriteNJetPlots:
     def __init__(self):    
         self.colors = [ROOT.kCyan+1, ROOT.kMagenta+1, ROOT.kYellow+1,
                        ROOT.kRed+1, ROOT.kGreen+1, ROOT.kBlue+1,
@@ -13,31 +23,25 @@ class MakeNJetPlots:
     def calcUnc(self,a,unc_a,b,unc_b):
         return  a/b * sqrt( (unc_a/a)**2+(unc_b/b)**2)
     
-    def makeNJplot(self,inputfiles, labels, signalfiles, signallabels, jettype, cut, outputfile):
-        basename = "h_njets_" + jettype
-        histos = [inputfile.Get(basename+"_"+cut) for inputfile in inputfiles]
-        # scale histos
-        for i,h in enumerate(histos):
-            #h.Scale(35900.)
-            h.SetName(h.GetName()+"_"+labels[i])
-            h.SetTitle(h.GetTitle()+"_"+labels[i])
+    def writeHistos(self, dataList, basename, cut):
+        histos = []
+        for dsi in dataList:
+            h = dsi.file.Get(basename+"_"+cut)
+            h.SetName(h.GetName()+"_"+dsi.label)
+            h.SetTitle(h.GetTitle()+"_"+dsi.label)
             h.Write()
-    
-        signalhistos = []
-        if signalfiles:
-            signalhistos = [inputfile.Get(basename+"_"+cut) for inputfile in signalfiles]
-            for i,h in enumerate(signalhistos):
-                #h.Scale(35900.)
-                h.SetName(h.GetName()+"_"+signallabels[i])
-                h.SetTitle(h.GetTitle()+"_"+signallabels[i])
-                h.Write()
-                
+            histos.append(h)
+        return histos
+
+    def makePseudoData(self, histos, signalhistos, sgDataList, jettype, cut):
         #make some pseudo_data
         mynewh = ROOT.TH1D(basename+"_"+cut+"_pseudodata", basename+"_"+cut+"_pseudodata",
                            histos[0].GetNbinsX(), histos[0].GetBinLowEdge(1), histos[0].GetBinLowEdge(1)+histos[0].GetNbinsX())
+        
         #make some pseudo_data with signal
         pseudodataS_histos = []
-        for sig in signallabels:
+        for dsi in sgDataList:
+            sig = dsi.label
             pseudodataS_histos.append( ROOT.TH1D(basename+"_"+cut+"_pseudodataS_"+sig, basename+"_"+cut+"_pseudodataS_"+sig,
                                                  histos[0].GetNbinsX(), histos[0].GetBinLowEdge(1), histos[0].GetBinLowEdge(1)+histos[0].GetNbinsX())
                                        )
@@ -49,21 +53,23 @@ class MakeNJetPlots:
                 #sumunc += h.GetBinError(bin+1)
             #sumdata = sumdata + sumunc*2*(random.random()-0.5)
             mynewh.SetBinContent(bin+1, int(round(sumdata)))
-    
+        
             for i, sigh in enumerate(signalhistos):
                 sumsig = sigh.GetBinContent(bin+1)
                 #sumsigunc = sigh.GetBinError(bin+1)
                 sumdatasigh = sumdata + sumsig #+ sumsigunc*2*(random.random()-0.5)
                 pseudodataS_histos[i].SetBinContent(bin+1, int(round(sumdatasigh)))
-    
+        
         mynewh.Write()
         for h in pseudodataS_histos:
             h.Write()
 
 class MakeDataCard:
-    def __init__(self, nMVABins, outFile):
+    def __init__(self, nMVABins, outFile, bgDataList, sgDataList):
         self.nMVABins = nMVABins
         self.outFile = outFile
+        self.bgDataList = bgDataList
+        self.sgDataList = sgDataList
 
     def printDataCard(self):
         print "Signal Region 1 Datacard -- signal category"
@@ -108,44 +114,44 @@ class MakeDataCard:
 if __name__ == "__main__":
     # Where the root files are stored
     basedir = "condor/rootfiles/"
-    # I hadd my ttbar files into TT.root, and I hadd all other backgrounds into BG_noTT.root
-    inputfilenames = [basedir + "TT.root", basedir + "BG_noTT.root"]
-    inputfiles = [ROOT.TFile.Open(inputfilename) for inputfilename in inputfilenames]
-    labels = ["TT", "other"]
-
-    signalfilenames = [basedir+"stealth_stop_350_SYY.root", 
-                       basedir+"stealth_stop_450_SYY.root", 
-                       basedir+"stealth_stop_550_SYY.root", 
-                       basedir+"stealth_stop_650_SYY.root", 
-                       basedir+"stealth_stop_750_SYY.root", 
-                       basedir+"stealth_stop_850_SYY.root", 
-                       basedir+"rpv_stop_350.root", 
-                       basedir+"rpv_stop_450.root", 
-                       basedir+"rpv_stop_550.root", 
-                       basedir+"rpv_stop_650.root", 
-                       basedir+"rpv_stop_750.root", 
-                       basedir+"rpv_stop_850.root", 
-                       ]
-    signalfiles = [ROOT.TFile.Open(signalfilename) for signalfilename in signalfilenames]
-    signallabels = ["SYY_350","SYY_450","SYY_550","SYY_650","SYY_750","SYY_850", 
-                    "RPV_350","RPV_450","RPV_550","RPV_650","RPV_750","RPV_850",]
-
+    cutlevels = [ "1l_deepESMbin1", "1l_deepESMbin2","1l_deepESMbin3","1l_deepESMbin4"]
+    jettypes = ["pt30"]#, "pt45"]
     outputfile = ROOT.TFile.Open("njets_for_Aron.root","RECREATE")
 
-    cutlevels = [
-        "1l_deepESMbin1", "1l_deepESMbin2","1l_deepESMbin3","1l_deepESMbin4",                 
-        ]
-    jettypes = ["pt30"]#, "pt45"]
+    # I hadd my ttbar files into TT.root, and I hadd all other backgrounds into BG_noTT.root
+    bgDataList = [
+        DataSetInfo(basedir, "TT.root",      "TT"),
+        DataSetInfo(basedir, "BG_noTT.root", "other"),
+    ]
 
-    mp = MakeNJetPlots()
+    sgDataList = [
+        DataSetInfo(basedir, "stealth_stop_350_SYY.root", "SYY_350"),
+        DataSetInfo(basedir, "stealth_stop_450_SYY.root", "SYY_450"),
+        DataSetInfo(basedir, "stealth_stop_550_SYY.root", "SYY_550"),
+        DataSetInfo(basedir, "stealth_stop_650_SYY.root", "SYY_650"),
+        DataSetInfo(basedir, "stealth_stop_750_SYY.root", "SYY_750"),
+        DataSetInfo(basedir, "stealth_stop_850_SYY.root", "SYY_850"),
+        DataSetInfo(basedir, "rpv_stop_350.root",         "RPV_350"),
+        DataSetInfo(basedir, "rpv_stop_450.root",         "RPV_450"),
+        DataSetInfo(basedir, "rpv_stop_550.root",         "RPV_550"),
+        DataSetInfo(basedir, "rpv_stop_650.root",         "RPV_650"),
+        DataSetInfo(basedir, "rpv_stop_750.root",         "RPV_750"),
+        DataSetInfo(basedir, "rpv_stop_850.root",         "RPV_850"),
+    ]
+
+    #Write all histos to outputfile
+    outputfile.cd()
+    wp = WriteNJetPlots()
     for jettype in jettypes:
+        basename = "h_njets_" + jettype
         for cut in cutlevels:
-            mp.makeNJplot(inputfiles, labels, signalfiles, signallabels, jettype, cut, outputfile)
-
+            histos = wp.writeHistos(bgDataList, basename, cut)
+            signalhistos = wp.writeHistos(sgDataList, basename, cut)
+            wp.makePseudoData(histos, signalhistos, sgDataList, jettype, cut)
+    
+    #Close outfile
     outputfile.Close()
-    for inputfile in inputfiles:
-        inputfile.Close()
-
+    
     #Make data card
-    md = MakeDataCard(nMVABins = 4, outFile = "multiF_ESM_ws.root")
+    md = MakeDataCard(nMVABins=4, outFile="multiF_ESM_ws.root", bgDataList=bgDataList, sgDataList=sgDataList)
     md.printDataCard()
