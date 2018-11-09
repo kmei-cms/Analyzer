a96d4d70f4904993039d85cc6aa7197c7a5bc782
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2018-11-08 18:04:45 -0600

    Added perNJet plots

--- Analyzer/test/sigMLBCut.py
+++ Analyzer/test/sigMLBCut.py
@@ -17,7 +17,10 @@ class DataSetInfo:
 
 def main():
     path = "condor/Analyze1Lep_Kerasv1.2.0/"
-    histoName = "h_mbl_1l_ge7j_ge1b_noMbl"
+    #path = "condor/Analyze1Lep_Kerasv1.2.0_noMCTrigger_PU_bTag_leptonWeight/"
+    #histoName = "h_mbl_1l_ge7j_ge1b_noMbl"
+    histoName = "h_njets_mbl_1l_ge7j_ge1b_noMbl"
+    rebinVal = 5
 
     bgData = {
         "TT"              : DataSetInfo(path=path, filename="TT.root",              sys=0.3, dataType="Background"),
@@ -32,61 +35,69 @@ def main():
 
     sgData = {
         "rpv_stop_350" : DataSetInfo(path=path, filename="rpv_stop_350.root", sys=-1.0, dataType="Signal"),
-        "rpv_stop_450" : DataSetInfo(path=path, filename="rpv_stop_450.root", sys=-1.0, dataType="Signal"),
-        "rpv_stop_550" : DataSetInfo(path=path, filename="rpv_stop_550.root", sys=-1.0, dataType="Signal"),
-        "rpv_stop_650" : DataSetInfo(path=path, filename="rpv_stop_650.root", sys=-1.0, dataType="Signal"),
-        "rpv_stop_750" : DataSetInfo(path=path, filename="rpv_stop_750.root", sys=-1.0, dataType="Signal"),
-        "rpv_stop_850" : DataSetInfo(path=path, filename="rpv_stop_850.root", sys=-1.0, dataType="Signal"),
+        #"rpv_stop_450" : DataSetInfo(path=path, filename="rpv_stop_450.root", sys=-1.0, dataType="Signal"),
+        #"rpv_stop_550" : DataSetInfo(path=path, filename="rpv_stop_550.root", sys=-1.0, dataType="Signal"),
+        #"rpv_stop_650" : DataSetInfo(path=path, filename="rpv_stop_650.root", sys=-1.0, dataType="Signal"),
+        #"rpv_stop_750" : DataSetInfo(path=path, filename="rpv_stop_750.root", sys=-1.0, dataType="Signal"),
+        #"rpv_stop_850" : DataSetInfo(path=path, filename="rpv_stop_850.root", sys=-1.0, dataType="Signal"),
     }
 
     # Loop over all background and get their histograms
     bgHistos = {}
     for key in bgData:
-        bgHistos[key] = {"hist" : bgData[key].getHisto(histoName)}
+        h = bgData[key].getHisto(histoName)
+        h.RebinY(rebinVal)
+        bgHistos[key] = {"hist" : h}
 
     # Loop over all signal models and calculate sig. for each one 
     for key in sgData:
         hSg = sgData[key].getHisto(histoName)
-        nBins = hSg.GetNbinsX()
-        low = hSg.GetXaxis().GetXmin()
-        high = hSg.GetXaxis().GetXmax()
+        hSg.RebinY(rebinVal)
+        nBins = hSg.GetNbinsY()
+        low = hSg.GetYaxis().GetXmin()
+        high = hSg.GetYaxis().GetXmax()
         binToGeV = (high - low)/nBins
-        c = ROOT.TCanvas( "c", "c", 0, 0, 800, 800)
-        sig2DHisto = ROOT.TH2D(key+"hist",key+"hist", nBins, low, high, nBins, low, high)
-
+        sigDic = {}
+            
         # Find all possible MLB cut values
-        for lowBin in range(1, nBins+1):            
-            for highBin in range(nBins+1):
-                if(lowBin < highBin):
-                    nSg = hSg.Integral(lowBin, highBin)
-                    
-                    # Loop over all background histos and calculate sigma
-                    sigma2 = 0.0
-                    for k in bgHistos:
-                        n = bgHistos[k]["hist"].Integral(lowBin, highBin)
-                        sys = ((bgData[k].sys)*n)**2
-                        sigma2 += n + sys
+        for nJetBin in range(6, hSg.GetNbinsX()+1):            
+            c2 = ROOT.TCanvas( "c", "c", 0, 0, 800, 800)
+            ROOT.gStyle.SetStatY(0.5)
+            ROOT.gStyle.SetStatX(0.85)
+            ROOT.gStyle.SetPalette(ROOT.kRainBow)
+            ROOT.gPad.SetLeftMargin(0.12)
+            ROOT.gPad.SetRightMargin(0.15)
+            ROOT.gPad.SetTopMargin(0.08)
+            ROOT.gPad.SetBottomMargin(0.12)
+            ROOT.gPad.SetTicks(1,1)
+            sig2DHisto = ROOT.TH2D(key+"hist"+str(nJetBin),key+"hist"+str(nJetBin), nBins, low, high, nBins, low, high)
+            for lowBin in range(1, nBins+1):       
+                for highBin in range(nBins+1):
+                    if(lowBin < highBin):
+                        nSg = hSg.Integral(nJetBin, nJetBin, lowBin, highBin)
+                        
+                        # Loop over all background histos and calculate sigma
+                        sigma2 = 0.0
+                        for k in bgHistos:
+                            n = bgHistos[k]["hist"].Integral(nJetBin, nJetBin, lowBin, highBin)
+                            sys = ((bgData[k].sys)*n)**2
+                            sigma2 += n + sys
+            
+                        # Calculate and fill sig.
+                        sig = 0.0
+                        if(sigma2 != 0): sig = nSg/math.sqrt(sigma2)
+                        sig2DHisto.SetBinContent(lowBin, highBin, sig)
+                        #print lowBin, highBin, nJetBin
+                        #sigDic[lowBin] = {highBin : {nJetBin : sig}}
+                        #sigDic[lowBin,highBin,nJetBin] = sig
 
-                    # Calculate and fill sig.
-                    sig = 0.0
-                    if(sigma2 != 0): sig = nSg/math.sqrt(sigma2)
-                    sig2DHisto.SetBinContent(lowBin, highBin, sig)
-
-        # Make the plot nice
-        ROOT.gStyle.SetStatY(0.5)
-        ROOT.gStyle.SetStatX(0.85)
-        ROOT.gStyle.SetPalette(ROOT.kRainBow)
-        ROOT.gPad.SetLeftMargin(0.12)
-        ROOT.gPad.SetRightMargin(0.15)
-        ROOT.gPad.SetTopMargin(0.08)
-        ROOT.gPad.SetBottomMargin(0.12)
-        ROOT.gPad.SetTicks(1,1)
-        sig2DHisto.SetTitle(key+" Significance")
-        sig2DHisto.GetXaxis().SetTitle("Lower Cut [GeV]")
-        sig2DHisto.GetYaxis().SetTitle("Higher Cut [GeV]")
-        sig2DHisto.Draw("colz")
-        c.SaveAs(key+"_sig.png")
-        del c
+            # Make the plot nice
+            sig2DHisto.SetTitle(key+" nJetBin "+str(nJetBin)+" Significance")
+            sig2DHisto.GetXaxis().SetTitle("Lower Cut [GeV]")
+            sig2DHisto.GetYaxis().SetTitle("Higher Cut [GeV]")
+            sig2DHisto.Draw("colz")
+            c2.SaveAs(key+"_sig_nJetBin"+str(nJetBin)+".png")
+            del c2                
 
 if __name__ == '__main__':
     main()
