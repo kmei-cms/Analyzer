cf8702f7182f750840033da387afc52b9f88e0e6
Semra Turkcapar <semra.turkcapar@cern.ch>
2019-09-10 14:34:59 -0500

    updated StealthHemisphers analzyer to compile

--- Analyzer/include/StealthHemispheres.h
+++ Analyzer/include/StealthHemispheres.h
@@ -4,40 +4,53 @@
 #include "TObject.h"
 #include "TLorentzVector.h"
 #include "TVector3.h"
+#include "TFile.h"
+#include "TFile.h"
+#include "TEfficiency.h"
+#include "TH1D.h"
+#include "TH2D.h"
 
-class StealthHemispheres : public TObject {
+#include <map>
+
+class NTupleReader;
+
+class StealthHemispheres 
+{
+private:
+    bool inithisto;
+    
 public:
-  StealthHemispheres();
-  virtual ~StealthHemispheres();
+    std::map<std::string, std::shared_ptr<TH1D>>  my_histos;
+    std::map<std::string, std::shared_ptr<TH2D>>  my_2d_histos;
+    std::map<std::string, std::shared_ptr<TEfficiency>>  my_efficiencies;
 
-  void Reset();
-  Int_t          seed_method;
-  Int_t          assoc_method;
-  Double_t       MT2;
-  Double_t       MCT;
-  Double_t       AlphaT;
-  Double_t       minDHT;
-  Double_t       maxDR;
-  Double_t       dPhi;
+    int          seed_method;
+    int          assoc_method;
+    double       MT2;
+    double       MCT;
+    double       AlphaT;
+    double       minDHT;
+    double       maxDR;
+    double       dPhi;
 
-  Int_t          jindices1  [m_jetSize];
-  Int_t          jindices2  [m_jetSize];
-  Int_t          eleindices1[m_eleSize];
-  Int_t          eleindices2[m_eleSize];
-  Int_t          muoindices1[m_muoSize];
-  Int_t          muoindices2[m_muoSize];
-  TLorentzVector lv1;
-  TLorentzVector lv2;
-  TLorentzVector UTM;
-
-  ClassDef(StealthHemispheres, 4)
-};
-
-class StealthHemispheres : public TObject {
+    std::vector<int> jindices1;
+    std::vector<int> jindices2;
+    std::vector<int> eleindices1;
+    std::vector<int> eleindices2;
+    std::vector<int> muoindices1;
+    std::vector<int> muoindices2;
+    TLorentzVector lv1;
+    TLorentzVector lv2;
+    TLorentzVector UTM;
 
     Double_t GetMT2Hemi(double testmass=0, bool massive=false, int PFJID=0, double minJPt=20, double maxJEta=2.4, int hemi_association=3, int met=1);
     Double_t CalcMT2(double testmass, bool massive, TLorentzVector visible1, TLorentzVector visible2, TLorentzVector MET); 
     Double_t HemiMassTop();
+
+    void Reset();
+    void Loop(NTupleReader& tr, double weight, int maxevents = -1, bool isQuiet = false);
+    void WriteHistos(TFile* outfile);
+    StealthHemispheres();
 
 };
 
