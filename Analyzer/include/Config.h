aef0f34f1f2e0a4987481a59b69d96e9697b4b6b
BryanCrossman <cros0400@umn.edu>
2020-07-23 18:23:22 -0500

    Add seed method parameter to MakeStopHemispheres call

--- Analyzer/include/Config.h
+++ Analyzer/include/Config.h
@@ -60,10 +60,10 @@ private:
             else if(module=="MegaJetCombine")               tr.emplaceModule<MegaJetCombine>();
             else if(module=="FatJetCombine")                tr.emplaceModule<FatJetCombine>();
             else if(module=="TrainingNTupleVars")          tr.emplaceModule<TrainingNTupleVars>();
-            else if(module=="MakeStopHemispheres_All")      tr.emplaceModule<MakeStopHemispheres>("Jets",     "AllJets",       "NJets",          "_All");
-            else if(module=="MakeStopHemispheres_1l")       tr.emplaceModule<MakeStopHemispheres>("Jets",     "GoodJets_pt30", "NGoodJets_pt30", "_1l");
-            else if(module=="MakeStopHemispheres_0l")       tr.emplaceModule<MakeStopHemispheres>("Jets",     "GoodJets_pt45", "NGoodJets_pt45", "_0l");
-            else if(module=="MakeStopHemispheres_TaggedTop") tr.emplaceModule<MakeStopHemispheres>("StopJets", "GoodStopJets",  "NGoodStopJets",  "_TaggedTop");
+            else if(module=="MakeStopHemispheres_All")      tr.emplaceModule<MakeStopHemispheres>("Jets",     "AllJets",       "NJets",          "_All", 2);
+            else if(module=="MakeStopHemispheres_1l")       tr.emplaceModule<MakeStopHemispheres>("Jets",     "GoodJets_pt30", "NGoodJets_pt30", "_1l", 2);
+            else if(module=="MakeStopHemispheres_0l")       tr.emplaceModule<MakeStopHemispheres>("Jets",     "GoodJets_pt45", "NGoodJets_pt45", "_0l", 2);
+            else if(module=="MakeStopHemispheres_TaggedTop") tr.emplaceModule<MakeStopHemispheres>("StopJets", "GoodStopJets",  "NGoodStopJets",  "_TaggedTop", 5);
             else if(module=="StopJets")                     tr.emplaceModule<StopJets>();
             else if(module=="DeepEventShape")               tr.emplaceModule<DeepEventShape>(DeepESMCfg, ModelFile);
             else if(module=="DeepEventShape_NonIsoMuon")    tr.emplaceModule<DeepEventShape>(DeepESMCfg_NonIsoMuon, ModelFile);
@@ -282,7 +282,7 @@ public:
                 "DeepEventShape",
                 "StopJets",
                 "MakeStopHemispheres_All",
-                //"MakeStopHemispheres_1l",
+                "MakeStopHemispheres_1l",
                 "MakeStopHemispheres_0l",
                 "MakeStopHemispheres_TaggedTop",
                 //"StopGenMatch",
