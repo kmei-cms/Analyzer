65218c0facc618add925aeee97e0781bfd1176c5
Semra <Semra.Turkcapar@cern.ch>
2020-10-08 18:19:45 -0500

    added header file for ISR gen matching

---
+++ Analyzer/include/ISRJets_Analyzer.h
@@ -0,0 +1,31 @@
+#ifndef ISRJets_Analyzer_h
+#define ISRJets_Analyzer_h
+
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TEfficiency.h>
+#include <TTree.h>
+#include <TH1F.h>
+
+#include <map>
+#include <string>
+
+class NTupleReader;
+
+class ISRJets_Analyzer 
+{
+public:
+    std::map<std::string, std::shared_ptr<TH1D>>  my_histos;
+    std::map<std::string, std::shared_ptr<TH2D>>  my_2d_histos;
+    std::map<std::string, std::shared_ptr<TEfficiency>>  my_efficiencies;
+    bool inithisto;
+ 
+    ISRJets_Analyzer();
+    ~ISRJets_Analyzer(){};
+    
+    void Loop(NTupleReader& tr, double weight, int maxevents = -1, bool isQuiet = false);
+    void InitHistos(const std::map<std::string, bool>& cutmap);
+    void WriteHistos(TFile* outfile);    
+};
+
+#endif
